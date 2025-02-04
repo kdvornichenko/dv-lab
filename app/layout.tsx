508eaec4a4c024d9a47c83d607ea05ba4d643926
Dvornichenko Kirill <gglade999@gmail.com>
2025-02-04 08:37:41 +0300

    Добавил аналитику

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,54 +1,57 @@
-import "@/styles/globals.css";
-import { Metadata, Viewport } from "next";
-import clsx from "clsx";
+import '@/styles/globals.css'
+import { Metadata, Viewport } from 'next'
+import clsx from 'clsx'
 
-import { Providers } from "./providers";
+import { Providers } from './providers'
 
-import { siteConfig } from "@/config/site";
-import { fontSans } from "@/config/fonts";
-import { Navbar } from "@/components/navbar";
+import { siteConfig } from '@/config/site'
+import { fontSans } from '@/config/fonts'
+import { Navbar } from '@/components/navbar'
+
+import { Analytics } from '@vercel/analytics/react'
 
 export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  icons: {
-    icon: "/favicon.ico",
-  },
-};
+	title: {
+		default: siteConfig.name,
+		template: `%s - ${siteConfig.name}`,
+	},
+	description: siteConfig.description,
+	icons: {
+		icon: '/favicon.ico',
+	},
+}
 
 export const viewport: Viewport = {
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-};
+	themeColor: [
+		{ media: '(prefers-color-scheme: light)', color: 'white' },
+		{ media: '(prefers-color-scheme: dark)', color: 'black' },
+	],
+}
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode
 }) {
-  return (
-    <html suppressHydrationWarning lang="en">
-      <head />
-      <body
-        className={clsx(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable,
-        )}
-      >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-          <div className="relative flex flex-col h-screen">
-            <Navbar />
-            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-              {children}
-            </main>
-          </div>
-        </Providers>
-      </body>
-    </html>
-  );
+	return (
+		<html suppressHydrationWarning lang='en'>
+			<head />
+			<body
+				className={clsx(
+					'min-h-screen bg-background font-sans antialiased',
+					fontSans.variable
+				)}
+			>
+				<Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
+					<Analytics />
+					<div className='relative flex flex-col h-screen'>
+						<Navbar />
+						<main className='container mx-auto max-w-7xl pt-16 px-6 flex-grow'>
+							{children}
+						</main>
+					</div>
+				</Providers>
+			</body>
+		</html>
+	)
 }
